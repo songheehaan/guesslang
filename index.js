b85e22ad9fe58491f054bf5cecc633f72a962498
songhee <songheekang3@gmail.com>
2017-08-07 23:09:03 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // importing
+const get = require("lodash.get");
 const express = require("express");
 const app = express();
-const axios = require("axios");
 
 //db
-var MongoClient = require("mongodb").MongoClient;
-var assert = require("assert");
-var ObjectId = require("mongodb").ObjectID;
-var url = "mongodb://localhost:27017/test";
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const ObjectId = require("mongodb").ObjectID;
 
+const DB_URL = "mongodb://localhost:27017/test";
 const API_KEY = "3534c04375dc49649473d58b7892e501";
+
+let db = null;
+MongoClient.connect(url, function(err, database) {
+  db = database;
+  findDocument(database, function() {
+    database.close();
+  });
+});
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
@@ -23,75 +31,57 @@ app.get("/", function(req, res) {
 //getting the text from clients text=djfsaljfdsalf
 app.get("/guess", function(req, res) {
   const query = req.query.text;
-  let language = "";
 
-  var findDocument = function(db, callback) {
-    var cursor = db.collection("guesslang").find({ text: query });
-    cursor.each(function(err, doc) {
-      assert.equal(err, null); // assert
-      if (doc != null) {
-        language = doc.language;
-        console.dir(language + " was found on our DB");
-        // show the user the language
-        res.send(language);
-      } else {
-        // send the text to API
-        axios({
-          method: "post",
-          url:
-            "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages",
-          headers: {
-            "Ocp-Apim-Subscription-Key": API_KEY,
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          },
-          data: {
-            documents: [
-              {
-                id: "test",
-                text: query
-              }
-            ]
-          }
-        })
-          .then(function(response) {
-            language = response.data.documents[0].detectedLanguages[0].name;
-            //save it in DB
-            var insertDocument = function(db, callback) {
-              db.collection("guesslang").insertOne({
-                text: query,
-                language: language
-              }, function(err, result) {
-                assert.equal(err, null);
-                console.log(
-                  query+" Inserted a document into the guesslang collection."
-                );
-                callback();
-              });
-            };
-            MongoClient.connect(url, function(err, db) {
-              assert.equal(null, err);
-              insertDocument(db, function() {
-                db.close();
-              });
-            });
+  fetchDb(db, query).then(lang => {
+    res.send(lang);
+    return;
+  });
 
-            //console.log(JSON.stringify(response.data));
-            res.send(language);
-          })
-          .catch(function(error) {
-            // console.error(error);
-            res.send("OOPS");
-          });
-        callback();
+  fetchService(query)
+    .then(lang => {
+      storeQuery(db, lang);
+      return lang;
+    })
+    .then(lang => {
+      res.send(lang);
+    });
+
+  const fetchDb = (db, query) => {
+    return db
+      .collection("guesslang")
+      .find({ text: query })
+      .toArray()
+      .then(documents => {
+        if (documents.length) {
+          return documents[0].language;
+        }
+      });
+  };
+
+  const fetchService = query => {
+    return axios({
+      method: "post",
+      url:
+        "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages",
+      headers: {
+        "Ocp-Apim-Subscription-Key": API_KEY,
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      data: {
+        documents: [{ id: "test", text: query }]
       }
+    }).then(response => {
+      return get(response, "data.documents[0].detectedLanguages[0].name");
     });
   };
 
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    findDocument(db, function() {
-      db.close();
+  const storeQuery = (db, language) => {
+    db.collection("guesslang").insertOne({
+      text: query,
+      language: language
     });
-  });
+  };
+
+  res.send("Did not undertand");
 });
